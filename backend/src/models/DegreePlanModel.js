4c73e933f75739740ce075bf2185d8d0eb21ed8e
Inbar Milstein <inbarmilstein@u.boisestate.edu>
2025-09-28 18:19:50 -0600

    [#72] update degree plan query

--- backend/src/models/DegreePlanModel.js
+++ backend/src/models/DegreePlanModel.js
@@ -13,7 +13,18 @@ const pool = require('../db');
 async function getDegreePlanByStudentId(studentId) {
     try {
         const result = await pool.query(
-            `SELECT * FROM degree_plans WHERE student_id = $1`,
+            `SELECT dp.plan_id, dp.course_status,
+                    c.course_id, c.course_code, c.course_name, c.credits,
+                    s.semester_id, s.semester_name, s.semester_type,
+                    s.sem_start_date, s.sem_end_date,
+                    p.program_id, p.program_name, p.program_type
+            FROM degree_plans dp
+            JOIN courses c ON dp.course_id = c.course_id
+            JOIN semesters s on dp.semester_id = s.semester_id
+            JOIN students st ON dp.student_id = st.student_id
+            JOIN programs p ON st.program_id = p.program_id
+            WHERE dp.student_id = $1
+            ORDER BY s.sem_start_date ASC`,
             [studentId]
         );
         // return all degree plan entries (courses in the plan) for the student
