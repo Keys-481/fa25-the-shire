024861c427e9e1d88f3085e5661c1e0d5f7ba588
Inbar Milstein <inbarmilstein@u.boisestate.edu>
2025-09-30 21:10:04 -0600

    update server and vite config to allow requests from frontend port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const BACKEND_URL = env.API_URL || `http://localhost:${env.PORT || 3000}`;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/students': {
+          target: BACKEND_URL,
+          changeOrigin: true,
+        },
+        '/courses': {
+          target: BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+});
