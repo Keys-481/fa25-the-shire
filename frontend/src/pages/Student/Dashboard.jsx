e9d63c2b80464434658aaa7f960f5b0be21c5cd9
anakamart <114611388+anakamart@users.noreply.github.com>
2025-10-02 18:37:40 -0600

    CSS on Student and Login View Is Consistent (#94)

--- frontend/src/pages/Student/Dashboard.jsx
+++ frontend/src/pages/Student/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/images/boise_state_wbg.png'
+import '../../styles/Styles.css'
 
 /**
  * StudentDashboard component displays the main dashboard for students with navigation options.
@@ -10,109 +11,35 @@ import logo from '../../assets/images/boise_state_wbg.png'
 export default function StudentDashboard() {
     const navigate = useNavigate()
 
-    // Defined color scheme
-    const backgroundColor = '#FFFFFF'
-    const textColor = '#FFFFFF' 
-    const secondaryTextColor = '#000000'
-
-    /**
-     * Styling for the Student Dashboard
-     */
-    const styles = {
-        container: {
-            backgroundColor: backgroundColor,
-            color: textColor,
-            minHeight: '100%',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        navbar: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '5px 20px',
-            backgroundColor: '#09347a',
-            borderBottom: '2px solid #f1632a',
-            height: '50px',
-        },
-        h2: {
-            margin: '2px',
-            marginLeft: '100px',
-            color: '#000000',
-            fontSize: '22px',
-            marginLeft: '60px',
-            fontFamily: 'Arial, sans-serif',
-        },
-        content: {
-            display: 'flex',
-            padding: '1rem',
-            backgroundColor: 'transparent',
-        },
-        buttonRow: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '2rem',
-        },
-        squareButton: {
-            width: '200px',
-            height: '200px',
-            backgroundColor: backgroundColor,
-            color: secondaryTextColor,
-            border: '1px solid ' + secondaryTextColor,
-            borderRadius: '8px',
-            fontSize: '1.2rem',
-            fontWeight: 'bold',
-            cursor: 'pointer',
-            display: 'flex',
-            justifyContent: 'center',
-            transition: 'transform 0.2s ease',
-            boxShadow: '0 8px 8px rgba(0, 0, 0, 0.1)',
-            padding: '1rem',
-        },
-        backButton: {
-            fontSize: '30px',
-            marginLeft: '10px',
-            cursor: 'pointer',
-            background: 'none',
-            border: 'none',
-            color: '#FFFFFF',
-        },
-    }
-
     return (
-        <div style={styles.container}>
+        <div>
             {/* Navigation bar */}
-            <div style={styles.navbar}>
-                            
-            <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-start' }}>
-                <button onClick={() => navigate('/')} style={styles.backButton}>←</button>
-            </div>  {/* remove or change to a log out button later*/}
-
-            <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-                <img src={logo} alt="BSU-Logo" style={{ height: '45px' }} />
+            <div className='navbar'>
+                <div style={{ position: 'absolute', left: '20px' }}>
+                    <button onClick={() => navigate('/')} className='back-button'>←</button>
+                </div>  {/* remove or change to a log out button later*/}
+                <img src={logo} alt="BSU-Logo" className='logo' />
             </div>
-            <div style={{ flex: 1 }}></div>
-        </div>
 
-            {/* Page Title */}
-            <h2 style={styles.h2}>Student Homepage</h2>
-
-            {/* Divider line */}
-            <div style={{ width: '100%', margin: '5px', height: '2px', backgroundColor: 'black' }}></div>
-
-
-            {/*  Main Content Area with Navigation Buttons */}
-            <div style={styles.content}>
-                <div style={styles.buttonRow}>
-                    {/* Navigation Button */}
-                    <button style={styles.squareButton} onClick={() => navigate('/student/degree-tracking')}>
-                        Degree Tracking
-                    </button>
-                    {/* Settings Button */}
-                    <button style={styles.squareButton} onClick={() => navigate('/student/settings')}>
-                        Settings
-                    </button>
+            <div className='window'>
+                {/* Page Title */}
+                <div className='title-bar'>
+                    <h1>Student Homepage</h1>
+                </div>
+                {/*  Main Content Area with Navigation Buttons */}
+                <div className='container'>
+                    <div className='dashboard-container'>
+                    <div className='button-row'>
+                        {/* Navigation Button */}
+                        <button className='square-button' onClick={() => navigate('/student/degree-tracking')}>
+                            Degree Tracking
+                        </button>
+                        {/* Settings Button */}
+                        <button className='square-button' onClick={() => navigate('/student/settings')}>
+                            Settings
+                        </button>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Student/Settings.jsx
+++ frontend/src/pages/Student/Settings.jsx
@@ -7,43 +7,15 @@ import StudentNavBar from "../../components/NavBars/StudentNavBar"
  * @returns {JSX.Element} A simple admin view for managing or viewing courses
  */
 export default function StudentSettings() {
-  // Defined color scheme
-      const backgroundColor = '#FFFFFF'
-      const textColor = '#FFFFFF' 
-      const secondaryTextColor = '#000000'
-  
-     /**
-     * Styling for the Settings page
-     */
-    const styles = {
-        container: {
-        backgroundColor,
-        color: textColor,
-        minHeight: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    h2: {
-        margin: '2px',
-        marginLeft: '100px',
-        color: '#000000',
-        fontSize: '22px',
-        marginLeft: '60px',
-        fontFamily: 'Arial, sans-serif',
-    },
-  }
-
   return (
-    <div style={styles.container}>
-        {/* Student Navigation Bar */}
-         <StudentNavBar/>
-         
-        {/* Title */}
-        <h2 style={styles.h2}>Student Settings</h2>
-
-       {/* Divider line */}
-        <div style={{ width: '100%', margin: '5px', height: '2px', backgroundColor: 'black' }}></div>
+    <div>
+      {/* Student Navigation Bar */}
+      <StudentNavBar />
+      <div className='window'>
+        <div className='title-bar'>
+          <h1>Settings</h1>
+        </div>
+      </div>
     </div>
   )
 }
