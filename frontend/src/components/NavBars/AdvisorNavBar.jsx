23d34f530cbbc19303090d30572664565345c259
Lauren Rausch <laurenrausch447@u.boisestate.edu>
2025-10-05 16:10:24 -0600

    Creating a secondary navigation bar

--- frontend/src/components/NavBars/AdvisorNavBar.jsx
+++ frontend/src/components/NavBars/AdvisorNavBar.jsx
@@ -4,14 +4,29 @@ import '../../styles/Styles.css'
 
 export default function AdvisorNavBar({ title }) {
   const navigate = useNavigate()
+  const location = useLocation()
 
-  {/* Display the Advisor navigation bar */}
+  const tabs =[
+    { id: 'advising', label: "Advising", path: '/advisor/advising' },
+    { id: 'compare', label: "Compare Catalogues", path: '/advisor/compare' },
+    { id: 'certificates-graduation', label: "Certificates/Graduation", path: '/advisor/certificates-graduation' },
+  ]
+  
+  // Display the Advisor navigation bar
   return (
-    <div className='navbar'>
-      <div style={{ position: 'absolute', left: '20px' }}>
-        <button onClick={() => navigate('/advisor/dashboard')} className='back-button'>←</button>
+    <>
+      {/* Header Navigation Bar */}
+      <div className='navbar'>
+        <div style={{ position: 'absolute', left: '20px' }}>
+          <button onClick={() => navigate('/advisor/dashboard')} className='back-button'>←</button>
+        </div>
+        <img src={logo} alt="BSU-Logo" className='logo'/>
       </div>
-      <img src={logo} alt="BSU-Logo" className='logo'/>
-    </div>
+
+      {/* Advising Tabs Nav Bar */}
+      <div className='tabs'>
+        
+      </div>
+    </>
   )
 }
